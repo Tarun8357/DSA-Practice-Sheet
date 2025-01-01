f75e75372a2b30ef0f4b1d0beffb0ff473128051
Tarun8357 <tarun.dhakad1205@gmail.com>
2025-01-01 18:56:05 +0530

    Added: map added in two sum

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -16,7 +16,7 @@ public class TwoSum {
         return null;
     }
 
-    public static int[] twoSum(int[] nums, int target) { 
+    public static int[] twoSum1(int[] nums, int target) { 
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
             map.put(nums[i], i);
@@ -28,6 +28,18 @@ public class TwoSum {
             }
         }
          
+        return null;
+    }
+
+    public static int[] twoSum(int[] nums, int target) { 
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int findMe = target - nums[i];
+            if(map.containsKey(findMe)){
+                return new int[]{i, map.get(findMe)};
+            }
+            map.put(nums[i],i);
+        }     
         return null;
     }
 

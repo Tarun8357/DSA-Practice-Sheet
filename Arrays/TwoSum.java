3f04bffbbd207b4043da58ae44344ffe18a22558
Tarun8357 <tarun.dhakad1205@gmail.com>
2025-01-01 18:52:48 +0530

    Added: map added in two sum

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,5 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
-    public static int[] twoSum(int[] nums, int target) { 
+    // Brute Force Approach
+    public static int[] twoSumBruteForce(int[] nums, int target) { 
         for(int i = 0 ; i < nums.length ; i++){
             for(int j = i+1; j < nums.length; j++ ){
                 if(nums[i]+nums[j]==target){
@@ -11,8 +15,24 @@ public class TwoSum {
          
         return null;
     }
+
+    public static int[] twoSum(int[] nums, int target) { 
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target-nums[i]) && map.get(target-nums[i]) != i){
+                return new int[]{i, map.get(target-nums[i])};
+            }
+        }
+         
+        return null;
+    }
+
     public static void main(String[] args) {
-        int nums [] = {2,6,5,4,7,4,5};
+        int nums [] = {2,6,5,4,7,4,};
         int target = 11;
         int[] twoSum = twoSum(nums, target);
         for (int i : twoSum) {
